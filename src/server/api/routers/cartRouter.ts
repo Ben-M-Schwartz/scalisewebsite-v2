c58558bb4e39cd5a6bf3968650de7d3c0c27ad26
bschwart33 <102708678+bschwart33@users.noreply.github.com>
2023-04-16 15:19:47 -0500

    Added updating, adding, and removing from cart from the cart page. Empty cart render fails

--- src/server/api/routers/cartRouter.ts
+++ src/server/api/routers/cartRouter.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-import { carts, cart_items } from '~/db/schema'
+import { carts, cart_items, product_details } from '~/db/schema'
 import { db } from '~/db/db'
 
 import { eq, and } from 'drizzle-orm/expressions';
@@ -13,53 +13,196 @@ import {
 
 export const cartRouter = createTRPCRouter({
   createCart: publicProcedure
-  .input(z.object({cart_id: z.string(), product_id: z.string(), price: z.number(),  quantity: z.string(), weight: z.number(), size:  z.string(), name: z.string() }))
+  .input(z.object({
+    cart_id: z.string(), 
+    product_id: z.string(), 
+    price: z.number(),  
+    quantity: z.string(), 
+    weight: z.number(), 
+    size:  z.string(), 
+    name: z.string() }))
   .mutation(async ({ input }) => {
-      await db.insert(carts).values({ cart_id: input.cart_id, total_price: (input.price * input.quantity), total_weight: (input.weight * input.quantity) })
-      await db.insert(cart_items).values({ cart_id: input.cart_id, product_id: input.product_id, price: (input.price * input.quantity).toString(), quantity: input.quantity, weight: input.weight, size: input.size, item_name: input.name })
+      await db.insert(carts)
+      .values({ 
+        cart_id: input.cart_id, 
+        total_price: (input.price * input.quantity), 
+        total_weight: (input.weight * input.quantity)
+      })
+      await db.insert(cart_items)
+      .values({ 
+        cart_id: input.cart_id, 
+        product_id: input.product_id, 
+        price: input.price * input.quantity, 
+        quantity: input.quantity, 
+        weight: input.weight, 
+        size: input.size, 
+        item_name: input.name 
+      })
     }),
 
   addToCart:  publicProcedure
-  .input(z.object({ cart_id: z.string(), product_id: z.string(), price:  z.number(),  quantity: z.number(), weight: z.number(), size:  z.string(), name: z.string() }))
+  .input(z.object({ 
+    cart_id: z.string(), 
+    product_id: z.string(), 
+    price:  z.number(),  
+    quantity: z.number(), 
+    weight: z.number(), 
+    size:  z.string(), 
+    name: z.string() }))
   .mutation(async ({ input }) => {
       const cartQuery = await db.select().from(carts).where(eq(carts.cart_id, input.cart_id))
       const cart = cartQuery[0]
       if(cart){
-        const itemQuery = await db.select().from(cart_items).where(and(eq(cart_items.cart_id, input.cart_id), eq(cart_items.product_id, input.product_id), eq(cart_items.size, input.size)))
+        const itemQuery = await db.select().from(cart_items)
+        .where(and(
+          eq(cart_items.cart_id, input.cart_id), 
+          eq(cart_items.product_id, input.product_id), 
+          eq(cart_items.size, input.size)
+          ))
         const cart_item = itemQuery[0]
         if(cart_item){
           const newQuantity = Number(cart_item.quantity) + input.quantity
-          await db.update(cart_items).set({ quantity: newQuantity}).where(and(eq(cart_items.cart_id, input.cart_id), eq(cart_items.product_id, input.product_id), eq(cart_items.size, input.size)))
-          await db.update(cart_items).set({ price: (input.price * newQuantity).toString() }).where(and(eq(cart_items.cart_id, input.cart_id), eq(cart_items.product_id, input.product_id), eq(cart_items.size, input.size)))
+          await db.update(cart_items)
+          .set({ quantity: newQuantity})
+          .where(and(
+            eq(cart_items.cart_id, input.cart_id), 
+            eq(cart_items.product_id, input.product_id), 
+            eq(cart_items.size, input.size)
+            ))
+          await db.update(cart_items)
+          .set({ price: (input.price * newQuantity).toString() })
+          .where(and(
+            eq(cart_items.cart_id, input.cart_id), 
+            eq(cart_items.product_id, input.product_id), 
+            eq(cart_items.size, input.size)
+            ))
         } else {
-          await db.insert(cart_items).values({ cart_id: input.cart_id, product_id: input.product_id, price: (input.price * input.quantity).toString(), quantity: input.quantity, weight: input.weight, size: input.size, item_name: input.name })
+          await db.insert(cart_items)
+          .values({ 
+            cart_id: input.cart_id, 
+            product_id: input.product_id, 
+            price: (input.price * input.quantity).toString(), 
+            quantity: input.quantity, 
+            weight: input.weight, 
+            size: input.size, 
+            item_name: input.name 
+          })
         }
         const newTotalPrice = Number(cart.total_price) + (input.price * input.quantity)
         const newTotalWeight = Number(cart.total_weight) + (input.weight * input.quantity)
-        await db.update(carts).set({ total_price: newTotalPrice, total_weight: newTotalWeight}).where(eq(carts.cart_id, input.cart_id))
+        await db.update(carts)
+        .set({ 
+          total_price: newTotalPrice, 
+          total_weight: newTotalWeight})
+          .where(eq(carts.cart_id, input.cart_id))
       } else {
-        await db.insert(carts).values({ cart_id: input.cart_id, total_price: (input.price * input.quantity), total_weight: (input.weight * input.quantity) })
-        await db.insert(cart_items).values({ cart_id: input.cart_id, product_id: input.product_id, price: (input.price * input.quantity).toString(), quantity: input.quantity, weight: input.weight, size: input.size, item_name: input.name })
+        await db.insert(carts)
+        .values({ 
+          cart_id: input.cart_id, 
+          total_price: (input.price * input.quantity), 
+          total_weight: (input.weight * input.quantity) 
+        })
+        await db.insert(cart_items)
+        .values({ 
+          cart_id: input.cart_id, 
+          product_id: input.product_id, 
+          price: (input.price * input.quantity).toString(), 
+          quantity: input.quantity, 
+          weight: input.weight, 
+          size: input.size, 
+          item_name: input.name })
       }
     }),
 
+  updateCart: publicProcedure
+  .input(z.object({
+    cart_id: z.string(),
+    name: z.string(),
+    size: z.string(),
+    quantity: z.number()
+  }))
+  .mutation(async ({ input }) => {
+    const cart = await db.select().from(carts).where(eq(carts.cart_id, input.cart_id))
+    const item_details = await db.select().from(product_details)
+    .where(and(
+      eq(product_details.name, input.name)
+    ))
+    if(cart){
+      const currentPrice = parseFloat(cart[0].total_price)
+      const currentWeight = parseFloat(cart[0].total_weight)
+      await db.update(cart_items)
+      .set({ quantity: input.quantity })
+      .where(and(
+        eq(cart_items.cart_id, input.cart_id), 
+        eq(cart_items.size, input.size), 
+        eq(cart_items.item_name, input.name)
+        ))
+      await db.update(cart_items)
+      .set({ price: item_details[0].price * input.quantity })
+      .where(and(
+        eq(cart_items.cart_id, input.cart_id),
+        eq(cart_items.size, input.size),
+        eq(cart_items.item_name, input.name)
+        ))
+      await db.update(carts)
+      .set({ 
+        total_price: currentPrice + parseFloat(item_details[0].price), 
+        total_weight: currentWeight + parseFloat(item_details[0].weight)
+      }).where(eq(carts.cart_id, input.cart_id))
+    } else {
+      throw new Error('Cart not found')
+    }
+  }),
+
   remove: publicProcedure
-    .input(z.object({ cart_id: z.string(), product_id: z.string(), size: z.string(), quantity: z.number(), fullRemove: z.boolean() }))
+    .input(z.object({ 
+      cart_id: z.string(), 
+      product_id: z.string(), 
+      size: z.string(), 
+      quantity: z.number(), 
+      fullRemove: z.boolean() }))
     .mutation(async ({ input }) => {
       const cart = await db.select().from(carts).where(eq(carts.cart_id, input.cart_id))
+      const item_details = await db.select().from(product_details)
+      .where(and(
+        eq(product_details.id, input.product_id)
+      ))
       if(cart){
+        const currentPrice = parseFloat(cart[0].total_price)
+        const currentWeight = parseFloat(cart[0].total_weight)
         if(input.fullRemove){
-          await db.delete(cart_items).where(and(eq(cart_items.cart_id, cart_id), eq(cart_items.product_id, input.product_id), eq(cart_items.size, input.size)))
-          await db.delete(carts).where(eq(carts.cart_id, input.cart_id))
+          await db.delete(cart_items)
+          .where(and(
+            eq(cart_items.cart_id, input.cart_id), 
+            eq(cart_items.product_id, input.product_id), 
+            eq(cart_items.size, input.size)
+            ))
+          await db.update(carts)
+          .set({
+            total_price: currentPrice - (parseFloat(item_details[0].price) * input.quantity),
+            total_weight: currentWeight - (parseFloat(item_details[0].weight) * input.quantity)
+          })
         } else {
-          const cart_item = await db.select().from(cart_items).where(and(eq(cart_items.cart_id, input.cart_id), eq(cart_items.product_id, input.product_id), eq(cart_items.size, input.size)))
-          if(cart_item){
-            await db.update(cart_items).set({ quantity: cart_item.quantity - input.quantity }).where(and(eq(cart_items.cart_id, input.cart_id), eq(cart_items.product_id, input.product_id), eq(cart_items.size, input.size)))
-            await db.update(carts).set({ total_price: cart.total_price - (input.price * input.quantity), total_weight: cart.total_weight - input.weight }).where(eq(carts.cart_id, input.cart_id))
-          } else {
-            throw new Error('Item not found')
-          }
-        }
+          await db.update(cart_items)
+          .set({ quantity: input.quantity })
+          .where(and(
+            eq(cart_items.cart_id, input.cart_id), 
+            eq(cart_items.size, input.size), 
+            eq(cart_items.product_id, input.product_id)
+            ))
+          await db.update(cart_items)
+          .set({ price: item_details[0].price * input.quantity })
+          .where(and(
+            eq(cart_items.cart_id, input.cart_id),
+            eq(cart_items.size, input.size),
+            eq(cart_items.product_id, input.product_id)
+            ))
+          await db.update(carts)
+          .set({ 
+            total_price: currentPrice - parseFloat(item_details[0].price), 
+            total_weight: currentWeight - parseFloat(item_details[0].weight)
+          }).where(eq(carts.cart_id, input.cart_id))
+      }
       } else {
         throw new Error('Cart not found')
       }
@@ -68,6 +211,20 @@ export const cartRouter = createTRPCRouter({
     getCart: publicProcedure
     .input(z.object({ cart_id: z.string() }))
     .query(async ({ input }) => {
-      return await db.select().from(carts).leftjoin('cart_items', eq(carts.cart_id, cart_items.cart_id)).where(eq(carts.cart_id, input.cart_id))
+      const result = await db.select({ 
+        cart_id: cart_items.cart_id, 
+        total_price: carts.total_price, 
+        total_weight: carts.total_weight, 
+        cart_item: { 
+          product_id: cart_items.product_id, 
+          price: cart_items.price, 
+          quantity: cart_items.quantity, 
+          size: cart_items.size, 
+          item_name: cart_items.item_name}
+        })
+        .from(carts)
+        .leftJoin(cart_items, eq(carts.cart_id, cart_items.cart_id))
+        .where(eq(carts.cart_id, input.cart_id))
+        return result
     })
 })
