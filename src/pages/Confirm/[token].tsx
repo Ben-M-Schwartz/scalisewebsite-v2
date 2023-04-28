0db95f8639f1785ecb34d3e959b5ade42d24c70d
Ben-M-Schwartz <benschwartz33@gmail.com>
2023-04-27 19:20:56 -0500

    why does this work locally but not on vercel

--- src/pages/Confirm/[token].tsx
+++ src/pages/Confirm/[token].tsx
@@ -22,13 +22,10 @@ const Confirm: NextPage = () => {
   const [loading, setLoading] = useState(true);
   const [invalidLink, setInvalid] = useState(true);
 
-  /*eslint-disable react-hooks/exhaustive-deps*/
   useEffect(() => {
     if (token) {
-      window.alert(token);
       subscribe(token)
         .then(() => {
-          window.alert("done");
           setLoading(false);
           setInvalid(false);
         })
@@ -38,8 +35,7 @@ const Confirm: NextPage = () => {
           setLoading(false);
         });
     }
-  }, [token]);
-  /*eslint-enabel react-hooks/exhaustive-deps*/
+  }, [token, subscribe]);
 
   return (
     <>
