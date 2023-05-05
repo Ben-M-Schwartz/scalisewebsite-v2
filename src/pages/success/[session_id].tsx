bb081b389b80556e5bcfc0f7f40f05ef2ccc8784
Ben-M-Schwartz <benschwartz33@gmail.com>
2023-05-05 10:58:03 -0500

    designed payment success page

--- src/pages/success/[session_id].tsx
+++ src/pages/success/[session_id].tsx
@@ -3,6 +3,8 @@ import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import Image from "next/image";
+import excited from "../../../public/excited.png";
 
 import { getCookie } from "cookies-next";
 import { api } from "~/utils/api";
@@ -33,10 +35,23 @@ const SuccessPage: NextPage = () => {
       <Head>
         <title>Confirmation</title>
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gray-800 text-white">
+      <main className="flex min-h-screen flex-col items-center justify-start bg-gray-800 text-white">
+        <div className="relative flex w-full justify-center bg-transparent">
+          <Image
+            src={excited}
+            alt="background photo"
+            fill
+            quality={75}
+            className="absolute z-0 object-cover object-[0%_15%]"
+            priority
+          />
+          <h1 className="z-10 py-24 text-center text-8xl text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+            SUCCESS
+          </h1>
+        </div>
         {session_id && (
           <>
-            <h1 className="text-center">Payment Successful!</h1>
+            <h1 className="pt-10 text-center">Payment Successful!</h1>
             <p className="text-center">Thank you for your purchase.</p>
             <p className="text-center">Check your email for order details</p>
           </>
