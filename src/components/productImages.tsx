20f549b911527194636ef4ebdc7734ffbee2f601
Ben-M-Schwartz <benschwartz33@gmail.com>
2023-05-08 11:11:46 -0500

    updated spacing for mobile

--- src/components/productImages.tsx
+++ src/components/productImages.tsx
@@ -20,7 +20,6 @@ const variants = {
   },
   exit: (direction: number) => {
     return {
-      //zIndex: 0,
       x: direction < 0 ? 550 : -550,
       opacity: 1,
       position: "absolute",
@@ -28,12 +27,6 @@ const variants = {
   },
 };
 
-/**
- * Experimenting with distilling swipe offset and velocity into a single variable, so the
- * less distance a user has swiped, the more velocity they need to register as a swipe.
- * Should accomodate longer swipes and short flicks without having binary checks on
- * just distance thresholds and velocity > 0.
- */
 const swipeConfidenceThreshold = 10000;
 const swipePower = (offset: number, velocity: number) => {
   return Math.abs(offset) * velocity;
@@ -41,11 +34,6 @@ const swipePower = (offset: number, velocity: number) => {
 
 export const Images = ({ images }: { images: string[] }) => {
   const [[page, direction], setPage] = useState([0, 0]);
-
-  // We only have 3 images, but we paginate them absolutely (ie 1, 2, 3, 4, 5...) and
-  // then wrap that within 0-2 to find our image ID in the array below. By passing an
-  // absolute page index as the `motion` component's `key` prop, `AnimatePresence` will
-  // detect it as an entirely new image. So you can infinitely paginate as few as 1 images.
 
   //eslint-disable-next-line
   const imageIndex = wrap(0, images.length, page);
@@ -58,7 +46,7 @@ export const Images = ({ images }: { images: string[] }) => {
     <>
       <div className="relative flex h-full w-2/3 flex-col md:w-1/2">
         <div className="flex flex-col md:flex-row">
-          <div className="flex w-1/3 flex-row max-md:order-1 md:flex-col lg:w-1/6">
+          <div className="flex w-1/3 flex-row max-md:order-1 md:w-1/6 md:flex-col">
             {images?.map((image, index) => (
               <button
                 key={index}
@@ -95,10 +83,6 @@ export const Images = ({ images }: { images: string[] }) => {
                     type: "tween",
                     duration: 0.7,
                     ease: "easeInOut",
-                    /*                     type: "spring",
-                    stiffness: 100,
-                    damping: 30,
-                    mass: 5, */
                   },
                   opacity: { duration: 0.7 },
                 }}
@@ -117,13 +101,13 @@ export const Images = ({ images }: { images: string[] }) => {
               />
             </AnimatePresence>
             <div
-              className="next absolute right-10 top-1/2 z-10 flex h-10 w-10 cursor-pointer select-none items-center justify-center rounded-full bg-white text-lg font-bold"
+              className="next absolute right-3 top-1/2 z-10 flex h-10 w-10 cursor-pointer select-none items-center justify-center rounded-full bg-white text-lg font-bold xl:right-10"
               onClick={() => paginate(1)}
             >
               {"‣"}
             </div>
             <div
-              className="prev absolute left-10 top-1/2 z-10 flex h-10 w-10 -scale-x-100 transform cursor-pointer select-none items-center justify-center rounded-full bg-white text-lg font-bold"
+              className="prev absolute left-3 top-1/2 z-10 flex h-10 w-10 -scale-x-100 transform cursor-pointer select-none items-center justify-center rounded-full bg-white text-lg font-bold xl:left-10"
               onClick={() => paginate(-1)}
             >
               {"‣"}
