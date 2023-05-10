7ea337a60b67a022a4c3e60cfac8c11cc940cef4
Ben-M-Schwartz <benschwartz33@gmail.com>
2023-05-10 12:47:06 -0500

    trying webhooks again

--- src/pages/api/webhooks/mailersend.ts
+++ src/pages/api/webhooks/mailersend.ts
@@ -1,33 +1,22 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import rawBody from "raw-body";
 
 import { db } from "~/db/db";
 import { subscribers } from "~/db/schema";
-import { type InferModel } from "drizzle-orm";
 import { eq } from "drizzle-orm/expressions";
-
-export const config = {
-  /* runtime: "edge", */
-  api: {
-    // Turn off the body parser so we can access raw body for verification.
-    bodyParser: false,
-  },
-};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  //const body = await rawBody(req);
-  //console.log(body)
-  console.log(req);
-  console.log(req.headers);
-  const signature = req.headers["mailersend-signature"] as string;
-
-  if (signature === process.env.MAILERSEND_SIGNATURE) {
-    //const requestBody = JSON.parse(body) as InferModel<typeof subscribers>;
-    //eslint-disable-next-line
-    await db.delete(subscribers).where(eq(subscribers.email, req.body.email));
+  //eslint-disable-next-line
+  if (!req.body.webhook_id || !req.body.data || !req.body.data.email)
+    return res.status(400).json({});
+  //eslint-disable-next-line
+  if (req.body.webhook_id === process.env.MAILERSEND_WEBHOOK_ID) {
+    await db
+      .delete(subscribers)
+      //eslint-disable-next-line
+      .where(eq(subscribers.email, req.body.data.email.recipient.email));
     return res.status(200).json({});
   }
 
