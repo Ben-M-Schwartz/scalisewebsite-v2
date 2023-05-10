80a3034c1497ba9d52fbd99a40e9fffb677a9fd4
Ben-M-Schwartz <benschwartz33@gmail.com>
2023-05-10 12:52:17 -0500

    trying webhooks again

--- src/pages/api/webhooks/mailersend.ts
+++ src/pages/api/webhooks/mailersend.ts
@@ -8,11 +8,12 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const signature = req.headers["mailersend-signature"] as string;
   //eslint-disable-next-line
   if (!req.body.webhook_id || !req.body.data || !req.body.data.email)
     return res.status(400).json({});
   //eslint-disable-next-line
-  if (req.body.webhook_id === process.env.MAILERSEND_WEBHOOK_ID) {
+  if (signature === process.env.MAILERSEND_WEBHOOK_ID) {
     await db
       .delete(subscribers)
       //eslint-disable-next-line
