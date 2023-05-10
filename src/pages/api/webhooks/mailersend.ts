d27f52f390611d3ce8f0090bb09918f7a9b84c3d
Ben-M-Schwartz <benschwartz33@gmail.com>
2023-05-10 13:01:14 -0500

    hopefully last webhook update again

--- src/pages/api/webhooks/mailersend.ts
+++ src/pages/api/webhooks/mailersend.ts
@@ -10,21 +10,17 @@ export default async function handler(
 ) {
   const signature = req.headers["mailersend-signature"] as string;
   //eslint-disable-next-line
-  if (!req.body.webhook_id || !req.body.data || !req.body.data.email)
+  if (!req.body.webhook_id || !req.body.data || !req.body.data.email) {
     return res.status(400).json({});
+  }
   //eslint-disable-next-line
   if (signature === process.env.MAILERSEND_WEBHOOK_ID) {
     await db
       .delete(subscribers)
       //eslint-disable-next-line
-      .where(eq(subscribers.email, req.body.data.email.recipient.email))
-      .then(() => {
-        return res.status(200).json({});
-      })
-      .catch(() => {
-        return res.status(400).json({ error: "An error deletign subscriber" });
-      });
+      .where(eq(subscribers.email, req.body.data.email.recipient.email));
+    return res.status(200).json({});
+  } else {
+    return res.status(400).json({});
   }
-
-  return res.status(400).json({});
 }
