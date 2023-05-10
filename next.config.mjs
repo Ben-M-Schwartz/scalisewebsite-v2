8d9c8f186d5d050ec3afb8c21719c6539a2e335c
Ben-M-Schwartz <benschwartz33@gmail.com>
2023-05-10 16:48:41 -0500

    disabled i18n

--- next.config.mjs
+++ next.config.mjs
@@ -42,10 +42,11 @@ const config = withAxiom({
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
-  i18n: {
+  //i18in seems to cause errors with vercel insights
+  /*   i18n: {
     locales: ["en"],
     defaultLocale: "en",
-  },
+  }, */
 });
 
 export default config;
