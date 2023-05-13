1e6f96db4487305c636503fb7180b733b4905afa
Ben-M-Schwartz <benschwartz33@gmail.com>
2023-05-13 18:53:41 -0500

    testign security headers

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,58 @@
  * This is especially useful for Docker builds.
  */
 import { withAxiom } from "next-axiom";
+import { createSecureHeaders } from "next-secure-headers";
+//import pkg from "webpack-subresource-integrity";
+//const { SubresourceIntegrityPlugin } = pkg;
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
 
 /** @type {import("next").NextConfig} */
 const config = withAxiom({
+  /*   webpack(config) {
+    config.output.crossOriginLoading = "anonymous";
+    config.plugins.push(
+      new SubresourceIntegrityPlugin({
+        hashFuncNames: ["sha256", "sha384"],
+        enabled: true,
+      })
+    );
+
+    return config;
+  }, */
+
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: createSecureHeaders({
+          contentSecurityPolicy: {
+            directives: {
+              defaultSrc: ["'self'"],
+              styleSrc: ["'self'", "'unsafe-inline'"],
+              imgSrc: ["'self'"],
+              baseUri: "self",
+              formAction: "self",
+              //eslint-disable-next-line
+              // @ts-ignore
+              frameAncestors: true,
+            },
+            frameGuard: "deny",
+            noopen: "noopen",
+            nosniff: "nosniff",
+            xssProtection: "sanitize",
+            forceHTTPSRedirect: [
+              true,
+              { maxAge: 60 * 60 * 24 * 360, includeSubDomains: true },
+            ],
+            referrerPolicy: "same-origin",
+          },
+        }),
+      },
+    ];
+  },
+
   async rewrites() {
     return [
       {
