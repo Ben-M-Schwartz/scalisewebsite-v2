ad55c7bb153b78668b292186683b87ccc93ab948
Ben-M-Schwartz <benschwartz33@gmail.com>
2023-07-14 15:04:21 -0500

    update to spacing on the music page

--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -1,7 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import Image from "next/image";
+import Image, { type StaticImageData } from "next/image";
 import { useState } from "react";
 
 import banner from "../../public/black_paper_1.webp";
@@ -18,6 +18,77 @@ import album1 from "../../public/fromNothingToNothing.webp";
 import album2 from "../../public/LP2_cover.webp";
 
 import { AudioPlayer } from "~/components/audioPlayer";
+
+const SingleLayout = ({
+  title,
+  order,
+  link,
+  image,
+  songsrc,
+  demotitle,
+  background,
+  background_color,
+}: {
+  title: string;
+  order: number;
+  link: string;
+  image: StaticImageData;
+  songsrc: string;
+  demotitle: string;
+  background: StaticImageData;
+  background_color: string;
+}) => {
+  return (
+    <div
+      className={`relative flex flex-col items-center gap-10 ${
+        background_color === "black"
+          ? "bg-stone-950 text-stone-100"
+          : "bg-stone-100 text-red-800"
+      } py-24`}
+    >
+      <div className="sm: z-10 flex w-full flex-col items-center justify-center gap-3 px-24 md:flex-row md:items-center md:justify-center md:gap-10 lg:w-5/6">
+        <div className="w-1/2 text-center md:order-1 md:w-1/2">
+          <div className="flex flex-col items-center justify-center gap-3">
+            <h1 className=" text-center text-4xl font-bold">{title}</h1>
+            <Link
+              rel="noopener noreferrer"
+              target="_blank"
+              className={`flex w-full flex-col justify-center rounded-sm border-4 py-2 text-center md:text-lg lg:text-xl ${
+                background_color === "black"
+                  ? "border-stone-100 hover:bg-stone-100 hover:font-bold hover:text-stone-950"
+                  : "border-red-800 text-red-800 hover:bg-red-800 hover:text-stone-100"
+              } md:w-1/2`}
+              href={link}
+            >
+              Listen Here
+            </Link>
+          </div>
+        </div>
+        <div
+          className={`${
+            order === 1 ? "order-1" : ""
+          } flex w-5/6 flex-col items-center justify-center md:w-1/2`}
+        >
+          <Image
+            src={image}
+            alt="Scalise Single Cover"
+            loading="lazy"
+            width={504}
+            height={504}
+            className="shadow-2xl shadow-red-900"
+          />
+        </div>
+      </div>
+      <AudioPlayer
+        //demo={true}
+        player_id={`${demotitle}Demo`}
+        source={songsrc}
+        title={demotitle}
+      />
+      <Image src={background} alt="background" className="absolute z-0" fill />
+    </div>
+  );
+};
 
 /* export const config = {
   runtime: "experimental-edge",
@@ -94,8 +165,9 @@ const Music: NextPage = () => {
 
         <div className="relative min-h-screen w-full">
           <div className="flex flex-col">
+            {/* */}
             <div className="relative flex flex-col items-center gap-10 bg-stone-100 py-24">
-              <div className="z-10 flex flex-col items-center justify-center gap-3 sm:px-24 md:flex-row md:items-center md:justify-center md:gap-10">
+              <div className="z-10 flex w-full flex-col items-center justify-center gap-3 sm:px-24 md:flex-row md:items-center md:justify-center md:gap-10 lg:w-5/6">
                 <div className="w-1/2 text-center md:order-1 md:w-1/2">
                   <div className="flex flex-col items-center justify-center gap-3">
                     <h1 className="text-center text-4xl font-bold text-red-800">
@@ -117,7 +189,7 @@ const Music: NextPage = () => {
                     </Link>
                   </div>
                 </div>
-                <div className="order-1 flex w-5/6 flex-col items-center md:w-1/2">
+                <div className="order-1 flex w-5/6 flex-col items-center justify-center md:w-1/2">
                   <Image
                     src={album2}
                     alt="Album 2 Cover"
@@ -136,219 +208,64 @@ const Music: NextPage = () => {
               />
             </div>
 
-            <div className="relative flex flex-col items-center gap-10 bg-stone-950 py-24">
-              <div className="z-10 flex flex-col items-center justify-center gap-3 sm:px-24 md:flex-row md:items-center md:justify-center md:gap-10">
-                <div className="w-1/2 text-center md:order-1 md:w-1/2">
-                  <div className="flex flex-col items-center justify-center gap-3">
-                    <h1 className="text-center text-4xl font-bold text-stone-100">
-                      ONE IN SEVENTY-EIGHT
-                    </h1>
-                    <Link
-                      rel="noopener noreferrer"
-                      target="_blank"
-                      className="flex w-full flex-col justify-center rounded-sm border-4 border-stone-100 py-2 text-center text-xl text-stone-100 hover:bg-stone-100 hover:font-bold hover:text-stone-950 md:w-1/2"
-                      href="https://distrokid.com/hyperfollow/scalise/one-in-seventy-eight"
-                    >
-                      Listen Here
-                    </Link>
-                  </div>
-                </div>
-                <div className="flex w-5/6 flex-col items-center md:w-1/2">
-                  <Image
-                    src={seventy}
-                    alt="One In Seventy-Eight Image"
-                    loading="lazy"
-                    width={504}
-                    height={504}
-                    className="shadow-2xl shadow-red-900"
-                  />
-                </div>
-              </div>
-              <AudioPlayer
-                //demo={true}
-                player_id="seventyeightDemo"
-                source="/One in Seventy-Eight Clip.mp3"
-                title="One In Seventy-Eight"
-              />
-              <Image
-                src={black_paper}
-                alt="background"
-                className="absolute z-0"
-                fill
-              />
-            </div>
+            <SingleLayout
+              title="ONE IN SEVENTY-EIGHT"
+              order={0}
+              link="https://distrokid.com/hyperfollow/scalise/one-in-seventy-eight"
+              image={seventy}
+              songsrc="/One in Seventy-Eight Clip.mp3"
+              demotitle="One In Seventy-Eight"
+              background={black_paper}
+              background_color="black"
+            />
 
-            <div className="relative flex flex-col items-center gap-10 bg-stone-100 py-24">
-              <div className="z-10 flex flex-col items-center justify-center gap-3 sm:px-24 md:flex-row md:items-center md:justify-center md:gap-10">
-                <div className="w-1/2 text-center md:order-1 md:w-1/2">
-                  <div className="flex flex-col items-center justify-center gap-3">
-                    <h1 className="text-center text-4xl font-bold text-red-800">
-                      SHE&apos;S GONNA KILL YOU
-                    </h1>
-                    <Link
-                      rel="noopener noreferrer"
-                      target="_blank"
-                      className="flex w-full flex-col justify-center rounded-sm border-4 border-red-800 py-2 text-center text-xl text-red-800 hover:bg-red-800 hover:text-stone-100 md:w-1/2"
-                      href="https://distrokid.com/hyperfollow/scalise/shes-gonna-kill-you"
-                    >
-                      Listen Here
-                    </Link>
-                  </div>
-                </div>
-                <div className="order-1 flex w-5/6 flex-col items-center md:w-1/2">
-                  <Image
-                    src={sgky}
-                    alt="She's Gonna Kill You Cover"
-                    loading="lazy"
-                    width={504}
-                    height={504}
-                    className="shadow-2xl shadow-red-900"
-                  />
-                </div>
-              </div>
-              <AudioPlayer
-                //demo={true}
-                player_id="sgkyDemo"
-                source="/She_s Gonna Kill You Clip.mp3"
-                title="She's Gonna Kill You"
-              />
-              <Image
-                src={white_paper}
-                alt="background"
-                className="absolute z-0"
-                fill
-              />
-            </div>
+            <SingleLayout
+              title="SHE'S GONNA KILL YOU"
+              order={1}
+              link="https://distrokid.com/hyperfollow/scalise/shes-gonna-kill-you"
+              image={sgky}
+              songsrc="/She_s Gonna Kill You Clip.mp3"
+              demotitle="She's Gonna Kill You"
+              background={white_paper}
+              background_color="white"
+            />
 
-            <div className="relative flex flex-col items-center gap-10 bg-stone-950 py-24">
-              <div className="z-10 flex flex-col items-center justify-center gap-3 sm:px-24 md:flex-row md:items-center md:justify-center md:gap-10">
-                <div className="w-1/2 text-center md:order-1 md:w-1/2">
-                  <div className="flex flex-col items-center justify-center gap-3">
-                    <h1 className="text-center text-4xl font-bold text-stone-100">
-                      TRANSFERRED TO HOUSTON
-                    </h1>
-                    <Link
-                      rel="noopener noreferrer"
-                      target="_blank"
-                      className="flex w-full flex-col justify-center rounded-sm border-4 border-stone-100 py-2 text-center text-xl text-stone-100 hover:bg-stone-100 hover:font-bold hover:text-stone-950 md:w-1/2"
-                      href="https://distrokid.com/hyperfollow/scalise/transferred-to-houston"
-                    >
-                      Listen Here
-                    </Link>
-                  </div>
-                </div>
-                <div className="flex w-5/6 flex-col items-center md:w-1/2">
-                  <Image
-                    src={transferred}
-                    alt="Transferred To Houston Image"
-                    loading="lazy"
-                    width={504}
-                    height={504}
-                    className="shadow-2xl shadow-red-900"
-                  />
-                </div>
-              </div>
-              <AudioPlayer
-                //demo={true}
-                player_id="transferredDemo"
-                source="/Transferred to Houston Clip.mp3"
-                title="Transferred To Houston"
-              />
-              <Image
-                src={black_paper}
-                alt="background"
-                className="absolute z-0"
-                fill
-              />
-            </div>
+            <SingleLayout
+              title="TRANSFERRED TO HOUSTON"
+              order={0}
+              link="https://distrokid.com/hyperfollow/scalise/transferred-to-houston"
+              image={transferred}
+              songsrc="/Transferred to Houston Clip.mp3"
+              demotitle="Transferred to Houston"
+              background={black_paper}
+              background_color="black"
+            />
 
-            <div className="relative flex w-full flex-col items-center gap-10 bg-stone-100 py-24">
-              <div className="z-10 flex flex-col items-center justify-center gap-3 md:flex-row md:gap-16">
-                <div className="w-1/2 text-center md:order-1 md:w-1/3">
-                  <div className="flex flex-col items-center justify-center gap-3">
-                    <h1 className="z-1 tex-center text-4xl font-bold text-red-800">
-                      AIRBAG
-                    </h1>
-                    <Link
-                      rel="noopener noreferrer"
-                      target="_blank"
-                      className="flex w-full flex-col justify-center rounded-sm border-4 border-red-800 py-2 text-center text-xl text-red-800 hover:bg-red-800 hover:font-bold hover:text-stone-100 md:w-1/2"
-                      href="https://distrokid.com/hyperfollow/scalise/airbag"
-                    >
-                      Listen Here
-                    </Link>
-                  </div>
-                </div>
-                <div className="order-1 flex w-5/6 flex-col items-center justify-center md:w-1/2">
-                  <Image
-                    src={airbag}
-                    alt="Airbag Image"
-                    width={504}
-                    height={504}
-                    className="shadow-2xl shadow-red-950"
-                    loading="lazy"
-                  />
-                </div>
-              </div>
-              <AudioPlayer
-                //demo={true}
-                player_id="airbagDemo"
-                source="/Airbag Clip.mp3"
-                title="Airbag"
-              />
-              <Image
-                src={white_paper}
-                alt="background"
-                className="absolute z-0"
-                fill
-              />
-            </div>
+            <SingleLayout
+              title="AIRBAG"
+              order={1}
+              link="https://distrokid.com/hyperfollow/scalise/airbag"
+              image={airbag}
+              songsrc="/Airbag Clip.mp3"
+              demotitle="Airbag"
+              background={white_paper}
+              background_color="white"
+            />
 
-            <div className="relative flex w-screen flex-col items-center gap-10 bg-stone-950 py-24">
-              <div className="z-10 flex flex-col items-center justify-center gap-3 sm:px-24 md:flex-row md:gap-0">
-                <div className="w-1/2 text-center md:order-1 md:w-1/2">
-                  <div className="flex flex-col items-center justify-center gap-3">
-                    <h1 className="z-1 tex-center text-4xl font-bold text-stone-100">
-                      FRUIT SNACKS
-                    </h1>
-                    <Link
-                      rel="noopener noreferrer"
-                      target="_blank"
-                      className="flex w-full flex-col justify-center rounded-sm border-4 border-stone-100 px-2 py-2 text-center text-xl text-stone-100 hover:bg-stone-100 hover:font-bold hover:text-stone-950 md:w-1/2"
-                      href="https://distrokid.com/hyperfollow/scalise/fruit-snacks"
-                    >
-                      Listen Here
-                    </Link>
-                  </div>
-                </div>
-                <div className="flex w-5/6 flex-col items-center justify-center md:w-1/2">
-                  <Image
-                    src={fruitSnacks}
-                    alt="Fruit Snacks Image"
-                    width={504}
-                    height={504}
-                    loading="lazy"
-                    className="shadow-2xl shadow-red-900"
-                  />
-                </div>
-              </div>
-              <AudioPlayer
-                //demo={true}
-                player_id="fruitSnacksDemo"
-                source="/Fruit Snacks Clip.mp3"
-                title="Fruit Snacks"
-              />
-              <Image
-                src={black_paper}
-                alt="background"
-                className="absolute z-0"
-                fill
-              />
-            </div>
+            <SingleLayout
+              title="FRUIT SNACKS"
+              order={0}
+              link="https://distrokid.com/hyperfollow/scalise/fruit-snacks"
+              image={fruitSnacks}
+              songsrc="/Fruit Snacks Clip.mp3"
+              demotitle="Fruit Snacks"
+              background={black_paper}
+              background_color="black"
+            />
 
-            <div className="relative flex w-screen flex-col items-center bg-red-800">
-              <div className="z-10 flex flex-col items-center justify-center gap-3 py-24 sm:px-24 md:flex-row md:items-center md:justify-center md:gap-10">
-                <div className="md:order-0 w-1/2 text-center">
+            <div className="relative flex flex-col items-center gap-10 bg-red-800 py-24">
+              <div className="z-10 flex w-full flex-col items-center justify-center gap-3 sm:px-24 md:flex-row md:items-center md:justify-center md:gap-10 lg:w-5/6">
+                <div className="md:order-0 w-1/2 text-center md:w-1/2">
                   <div className="flex flex-col items-center justify-center gap-3">
                     <h1 className="text-center text-4xl font-bold text-stone-100">
                       FROM NOTHING TO NOTHING
